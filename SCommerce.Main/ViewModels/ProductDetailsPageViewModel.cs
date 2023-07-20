5e0f92e6309c8ca007d02ba7087ec6be8e6f09f1
Carolina Melo <caroolpmelo@gmail.com>
2023-07-20 15:59:51 -0300

    Removes unused usings and adds ViewModel page, adding dinamic bind for some components.

---
+++ SCommerce.Main/ViewModels/ProductDetailsPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SCommerce.Main.ViewModels
+{
+    public class ProductDetailsPageViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Rating { get; set; }
+        public List<string> Images { get; set; }
+        public string SelectedImage { get; set; }
+
+        public ProductDetailsPageViewModel()
+        {
+            Title = "Great Product";
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse magna turpis, egestas sed leo ut, blandit porttitor ligula. Vivamus a sodales dui.";
+            Price = 99.99;
+            Rating = 4;
+            Images = new List<string>
+            {
+                "ms-appx:///Assets/Images/shirt1.jpg",
+                "ms-appx:///Assets/Images/shirt2.jpg",
+                "ms-appx:///Assets/Images/shirt3.jpg",
+            };
+            SelectedImage = Images[0];
+        }
+    }
+}
