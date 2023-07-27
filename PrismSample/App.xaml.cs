97396fa4903f4ef02efdeebc50fbfb0807c3a677
Carolina Melo <caroolpmelo@gmail.com>
2023-07-27 13:53:28 -0300

    Adds a prism sample project

---
+++ PrismSample/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Unity.Windows;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace PrismSample
+{
+    sealed partial class App : PrismUnityApplication
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Welcome", null);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ PrismSample/ViewModels/WelcomePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Windows.Mvvm;
+
+namespace PrismSample.ViewModels
+{
+    public class WelcomePageViewModel : ViewModelBase
+    {
+        private int counter = 0;
+        public int Counter
+        {
+            get { return counter; }
+            set { SetProperty(ref counter, value); }
+        }
+    }
+}
