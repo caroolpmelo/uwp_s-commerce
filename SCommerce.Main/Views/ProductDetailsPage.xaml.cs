52af13a57c7e8a79788ae35ac7d76f2e6d2af98b
Carolina Melo <caroolpmelo@gmail.com>
2023-07-28 15:43:24 -0300

    Run code cleanup on SCommerce.main project

--- SCommerce.Main/Views/ProductDetailsPage.xaml.cs
+++ SCommerce.Main/Views/ProductDetailsPage.xaml.cs
@@ -6,7 +6,7 @@ namespace SCommerce.Main.Views
     public sealed partial class ProductDetailsPage : Page
     {
         public ProductDetailsPageViewModel ViewModel => (ProductDetailsPageViewModel)this.DataContext;
-        
+
         public ProductDetailsPage()
         {
             this.InitializeComponent();
